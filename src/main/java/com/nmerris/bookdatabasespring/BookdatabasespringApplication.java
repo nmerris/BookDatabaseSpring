84ed3e3f9a78ff2692dea9d466cc3d86c3271d01
nmerris <nate.merris@gmail.com>
2017-08-01 16:54:41 -0400

    initial commit

---
+++ src/main/java/com/nmerris/bookdatabasespring/BookdatabasespringApplication.java
@@ -0,0 +1,12 @@
+package com.nmerris.bookdatabasespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookdatabasespringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookdatabasespringApplication.class, args);
+	}
+}
---
+++ src/main/java/com/nmerris/bookdatabasespring/controllers/MainController.java
@@ -0,0 +1,61 @@
+package com.nmerris.bookdatabasespring.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+
+    @RequestMapping("/")
+    public String defaultPage(Model model) {
+        // below 'message' must match something in addproduct.html
+        model.addAttribute("message", "Welcome to the book database");
+        return "defaultpage"; // must be the name of the html file
+    }
+
+    // search by category
+    @RequestMapping("/findbooks")
+    public String findBooks(Model model) {
+        model.addAttribute("message", "Select category");
+        return "findbooks";
+    }
+
+
+    @RequestMapping("/findbooksbyauthor")
+    public String findBooksByAuthor(Model model) {
+        model.addAttribute("message", "Select author");
+        return "findbooksbyauthor";
+    }
+
+
+    @RequestMapping("/findbooksbysku")
+    public String findBooksBySku(Model model) {
+        model.addAttribute("message", "Select SKU");
+        return "findbooksbysku";
+    }
+
+
+    @RequestMapping("/addbook")
+    public String addBook(Model model) {
+        model.addAttribute("message", "This is where to add a book");
+        return "addbook";
+    }
+
+
+    @RequestMapping("/deletebook")
+    public String deleteBook(Model model) {
+        model.addAttribute("message", "This is where to delete books");
+        return "deletebook";
+    }
+
+
+    @RequestMapping("/showallbooks")
+    public String showAllBooks(Model model) {
+        model.addAttribute("message", "This is a list of all books");
+        return "showallbooks";
+    }
+
+}
